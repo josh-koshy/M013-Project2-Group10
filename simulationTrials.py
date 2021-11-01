b285535695ae144ea267cca5b4f8cb8cc1de1a7c
danielchm6 <91968568+danielchm6@users.noreply.github.com>
2021-11-01 19:12:08 -0400

    Add files via upload
    exploit, explore, and simulation runner functions

---
+++ simulationTrials.py
@@ -0,0 +1,26 @@
+from exploitOnly import *
+from exploreOnly import *
+
+def Simulation(t):
+    oH = 300*11
+    print("Optimum Happiness -> " + str(oH))
+    eTH = 9 + 7 + 11 + 297*11 # eTH -> expected total happiness
+    print("Expected Total Happiness -> " + str(eTH))
+    regret = oH - eTH
+    print("Expected Regret -> " + str(regret))
+    print("\nData after " + str(t) + " trials:")
+    numTrials = 0
+    totalHappiness = 0
+    totalRegret = 0
+    while numTrials != t:
+        h = happinessSum()
+        totalHappiness = totalHappiness + h
+        regret = oH - h
+        totalRegret = totalRegret + regret
+        numTrials += 1
+    avgTH = totalHappiness / t # avgTH -> average total happiness
+    print("Average Total Happiness -> " + str(avgTH))
+    avgRegret = totalRegret / t
+    print("Average Regret -> " + str(avgRegret))
+
+Simulation(10000)
