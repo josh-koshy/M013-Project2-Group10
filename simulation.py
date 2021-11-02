07266dbd72bbfdc6b38948859a1d69b25bb751ff
Dan <91968568+danielchm6@users.noreply.github.com>
2021-11-01 20:13:33 -0400

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,27 +1,50 @@
-#Simulation Function
-from exploitOnly import *
+from exploitOnlyFinal import *
 from exploreOnly import *
 
-def Simulation(t):
-    oH = 300*11
+def Simulation(t: int):
+    print("\nData after " + str(t) + " trials..\n")
+    print("Exploit only (Best Cafe = " + str(bestCafe) + "):")
+    oH = 300 * 11
     print("Optimum Happiness -> " + str(oH))
-    eTH = 9 + 7 + 11 + 297*11 # eTH -> expected total happiness
+    eTH = 9 + 7 + 11 + 297 * 11  # eTH -> expected total happiness
     print("Expected Total Happiness -> " + str(eTH))
     regret = oH - eTH
     print("Expected Regret -> " + str(regret))
-    print("\nData after " + str(t) + " trials:")
     numTrials = 0
     totalHappiness = 0
     totalRegret = 0
     while numTrials != t:
-        h = happinessSum()
-        totalHappiness = totalHappiness + h
-        regret = oH - h
+        h300 = exploit()
+        totalHappiness = totalHappiness + h300
+        regret = oH - h300
         totalRegret = totalRegret + regret
         numTrials += 1
     avgTH = totalHappiness / t # avgTH -> average total happiness
     print("Average Total Happiness -> " + str(avgTH))
     avgRegret = totalRegret / t
     print("Average Regret -> " + str(avgRegret))
+
+    print("\nExplore Only:")
+    oH = 300 * 11
+    print("Optimum Happiness -> " + str(oH))
+    eTH = 100*9 + 100*7 + 100*11  # eTH -> expected total happiness
+    print("Expected Total Happiness -> " + str(eTH))
+    regret = oH - eTH
+    print("Expected Regret -> " + str(regret))
+    numTrials = 0
+    totalHappiness = 0
+    totalRegret = 0
+    while numTrials != t:
+        h300 = explore()
+        totalHappiness = totalHappiness + h300
+        regret = oH - h300
+        totalRegret = totalRegret + regret
+        numTrials += 1
+    avgTH = totalHappiness / t  # avgTH -> average total happiness
+    print("Average Total Happiness -> " + str(avgTH))
+    avgRegret = totalRegret / t
+    print("Average Regret -> " + str(avgRegret))
+
+    print("\neGreedy:")
 
 Simulation(10000)
