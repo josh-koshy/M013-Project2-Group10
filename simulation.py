8b9e8237b6279fd5d021ecccc92b799f3f37454c
Dan <91968568+danielchm6@users.noreply.github.com>
2021-11-03 08:38:34 -0400

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,16 +1,18 @@
+#Simulation
 from exploitOnly import *
 from exploreOnly import *
+from eGreedy import *
 
-def Simulation(t: int):
+def Simulation(t: int, e: int): # e is value for e greedy
     print("\nData after " + str(t) + " trials..\n")
     print("Exploit only:")
-    oH = 300 * 11
+    oH = 300 * 11 # oH -> optimum happiness
     print("Optimum Happiness -> " + str(oH))
     if findBestCaf() == 1:
         eTH = 9 + 7 + 11 + (297 * 9)
-    if findBestCaf() == 2:
+    elif findBestCaf() == 2:
         eTH = 9 + 7 + 11 + (297 * 7)
-    if findBestCaf() == 3:
+    elif findBestCaf() == 3:
         eTH = 9 + 7 + 11 + (297 * 11)
     # eTH -> expected total happiness
     print("Expected Total Happiness -> " + str(eTH))
@@ -48,7 +50,26 @@ def Simulation(t: int):
     print("Average Regret -> " + str(avgRegret))
 
     print("\neGreedy:")
+    oH = 300 * 11
+    print("Optimum Happiness -> " + str(oH))
+    eDecimal = e/100
+    eTH = (1-eDecimal)*300*11 + (eDecimal/3)*300*9 + (eDecimal/3)*300*7 + (eDecimal/3)*300*11  # eTH -> expected total happiness
+    print("Expected Total Happiness -> " + str(eTH))
+    regret = oH - eTH
+    print("Expected Regret -> " + str(regret))
+    totalHappiness = 0
+    totalRegret = 0
+    for i in range(t):
+        h300 = eGreedy(e)
+        totalHappiness = totalHappiness + h300
+        regret = oH - h300
+        totalRegret = totalRegret + regret
+    avgTH = totalHappiness / t  # avgTH -> average total happiness
+    print("Average Total Happiness -> " + str(avgTH))
+    avgRegret = totalRegret / t
+    print("Average Regret -> " + str(avgRegret))
 
-Simulation(1000)
-Simulation(10000)
-Simulation(100000)
+
+Simulation(100, 1)
+Simulation(10000, 20)
+Simulation(100000, 20)
